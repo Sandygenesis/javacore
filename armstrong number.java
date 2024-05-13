01dc51bcb2a38e12b02c1913c988ddc707850caa
Sandygenesis <168648141+Sandygenesis@users.noreply.github.com>
2024-05-13 11:09:13 +0530

    Update armstrong number.java

--- armstrong number.java
+++ armstrong number.java
@@ -20,26 +20,3 @@ public class amst {
 
 }
 }
-*/
-package examples;
-public class amst {
-	public static void main(String[] args) {
-	int r,n=121,tem;
-	int sum=0;
-	tem=n;
-	while(n>0)
-	{
-		r=n%10;
-		n=n/10;
-		sum=(sum*10)+r;
-	}
-	if(tem==sum)
-	{
-		System.out.println("palindrom");
-	
-	}
-	else
-		System.out.println("not palidrom");
-
-}
-}
